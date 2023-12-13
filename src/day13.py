770ce89afd60034daac5b23fecd9e62868f88c20
Konrad Stepniak <vesphy@gmail.com>
2023-12-13 13:14:17 +0700

    Day 13

--- src/day13.py
+++ src/day13.py
@@ -86,9 +86,83 @@ def find_reflection(pattern):
     # print(col_sum, row_sum)
     return col_sum + row_sum
 
+
+def find_reflection_smudge(pattern):
+    rows = len(pattern)
+    cols = len(pattern[0])
+
+    row_sum = 0
+    different_idx = -1
+    for row_idx in range(rows):
+        # print("Checking ", row_idx, rows)
+
+        different_values = 0
+
+        all_columns = True
+        for col_idx in range(cols):
+            up_idx = row_idx - 1
+            down_idx = row_idx
+            equal = True
+            while up_idx >= 0 and down_idx < rows:
+                if pattern[up_idx][col_idx] != pattern[down_idx][col_idx]:
+                    equal = False
+                    different_values += 1
+                    # break
+                up_idx -= 1
+                down_idx += 1
+
+            if not equal:
+                all_columns = False
+                # break
+
+        if different_values == 1:
+            different_idx = row_idx
+            print("different row: values: ", different_values)
+        # if all_columns and (up_idx == 0 and down_idx == rows) or (up_idx == -1 and down_idx == rows - 1):
+        if all_columns:
+            # print("FOUND A BREAKING POINT: ", row_idx, up_idx, down_idx, rows)
+            row_sum += row_idx * 100
+            # return row_idx * 100
+
+    if different_idx != -1:
+        return different_idx * 100
+
+    col_sum = 0
+    for col_idx in range(cols):
+        # print("Col checking ", col_idx, cols)
+
+        different_values = 0
+        all_rows = True
+        for row_idx in range(rows):
+            left_idx = col_idx - 1
+            right_idx = col_idx
+            equal = True 
+            while left_idx >= 0 and right_idx < cols:
+                if pattern[row_idx][left_idx] != pattern[row_idx][right_idx]:
+                    different_values += 1
+                    equal = False
+                left_idx -= 1
+                right_idx += 1
+            
+            if not equal:
+                all_rows = False
+
+        if different_values == 1:
+            different_idx = col_idx
+            print("different column values: ", different_values)
+        # if all_rows and (left_idx == 0 and right_idx == cols) or (left_idx == -1 and right_idx == cols - 1):
+        if all_rows:
+            # print("COLUMN BREAKING POINT: ", col_idx, left_idx, right_idx, cols)
+            col_sum += col_idx
+
+    if different_idx != -1:
+        return different_idx
+    # print(col_sum, row_sum)
+    return col_sum + row_sum
+
 ans = 0
 for idx, pattern in enumerate(patterns):
     # print(idx)
-    ans += find_reflection(pattern)
+    ans += find_reflection_smudge(pattern)
 
 print("part1: ", ans)

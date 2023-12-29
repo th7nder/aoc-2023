d4de245903f19633380dc486330728308e2004ea
Konrad Stepniak <vesphy@gmail.com>
2023-12-29 13:06:05 +0100

    Add Day 21 part , refactored

--- src/day21.py
+++ src/day21.py
@@ -1,7 +1,7 @@
 
 garden = []
 
-with open("input/21_small.txt") as f:
+with open("input/21.txt") as f:
     for line in f:
         line = line.strip()
         if line:
@@ -43,58 +43,42 @@ from collections import deque
 
 print_garden(garden, visited=set())
 
-def part1(garden, max_steps=6):
+def fill(garden, max_steps=6):
     rows, cols, start = preprocess(garden)
     def is_valid(new_pos):
      return new_pos[0] >= 0 and new_pos[0] < rows and new_pos[1] >= 0 and new_pos[1] < cols
 
 
-    visited = set((start, 0, 0))
-    queue = deque([(start, max_steps, 0, 0)])
-    haha = set()
+    visited = set((start))
+    queue = deque([(start, max_steps)])
+    ans = set()
 
     while queue:
-        curr, s, rm, cm = queue.popleft()
+        curr, s = queue.popleft()
 
         if s % 2 == 0:
-            haha.add((curr, rm, cm))
+            ans.add(curr)
 
         if s == 0:
             continue
+
         for dir in [TOP, DOWN, RIGHT, LEFT]:
             new_pos = add(curr, dir)
-            
-            # part 2
-            if new_pos[0] >= rows:
-                new_pos = (0, new_pos[1])
-                rm += 1
-            elif new_pos[0] < 0:
-                new_pos = (rows - 1, new_pos[1])
-                rm -= 1
-            
-            if new_pos[1] >= cols:
-                new_pos = (new_pos[0], 0)
-                cm += 1
-            elif new_pos[1] < 0:
-                new_pos = (new_pos[0], cols - 1)
-                cm -= 1
-
             (new_r, new_c) = new_pos
 
-            # if not is_valid(new_pos):
-                # continue
+            if not is_valid(new_pos):
+                continue
+
             if garden[new_r][new_c] == '#':
                 continue
             
-            if (new_pos, rm, cm) in visited:
+            if (new_pos) in visited:
                 continue
 
-            visited.add((new_pos, rm, cm))
-            queue.append((new_pos, s - 1, rm, cm))
+            visited.add((new_pos))
+            queue.append((new_pos, s - 1))
 
-    return haha
+    return ans
 
-visited = part1(garden, 10)
-# print_garden(garden, visited)
-print(len(visited))
+print("Part 1: ", len(fill(garden, 64)))
 
